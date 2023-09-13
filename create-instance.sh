d1e2b9a713631268ada71b7e43353e2edc5eeb3c
Aviraj Khare <49766964+avirajkhare00@users.noreply.github.com>
2023-09-13 11:36:04 +0530

    Update create-instance.sh
    experimenting with this magic number

--- create-instance.sh
+++ create-instance.sh
@@ -10,7 +10,7 @@ instance_response=$(aws ec2 run-instances \
     --user-data file://provision.txt \
     --block-device-mappings 'DeviceName=/dev/sda1,Ebs={VolumeSize=28,VolumeType=gp2,DeleteOnTermination=true,Encrypted=true}' \
     --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value="CloudGenius Workstation"}]' 'ResourceType=volume,Tags=[{Key=Name,Value="Disk for Cloud Genius"}]')
-sleep 120
+sleep 10
 instanceId=$(echo -e "$instance_response" |  jq -r '.Instances[] | .InstanceId' | tr -d '"')
 PublicIpAddress=$(aws ec2 describe-instances \
     --instance-id $instanceId | jq -r '.Reservations[] | .Instances[] | .PublicIpAddress' | tr -d '"')
